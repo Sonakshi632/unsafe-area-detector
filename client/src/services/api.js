b7319f7e1b4aef3ee397a6e7a4eb74a39deec256
Sonakshi Shukla <ssonakshi754@gmail.com>
2026-04-25 19:10:32 +0530

    Fix api.js syntax error

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://unsafe-area-detector.onrender.com/api" 
+  baseURL: "https://unsafe-area-detector.onrender.com/api"
+});
 
-// Attach token to every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
